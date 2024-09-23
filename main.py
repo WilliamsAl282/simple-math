40ce3da1c758cdf5eaedc2e775b49ec495354dda
WilliamsAl282 <WilliamsAl282@northwested.org>
2024-09-23 11:17:34 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+# Alex Williams
+# 9/23/24
+# basic math
+
+# Practice 1
+num1 = 10
+num2 = 15
+
+sum = (num2 + num1)
+subtraction = (num2 - num1)
+multiplication = (num1 * num2)
+quotient_remainder = (num1 / num2)
+
+print(f'{num1} plus {num2} equals {sum}')
+print(f'{num2} minus {num1} equals {subtraction}')
+print(f'{num1} times {num2} equals {multiplication}')
+print(f'{num1} divided by {num2} equals {quotient_remainder}')
+
+
+
+
